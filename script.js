01e0a5eae09b9ffb18dbc99abfffc619425ee688
Sandra <oyarekhuas@gmail.com>
2022-09-29 02:30:18 +0100

    add button complete

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-add= document.getElementById('add')
-calc= document.getElementById('calc')
-remove= document.getElementById('remove')
+var add= document.getElementById('add')
+var calc= document.getElementById('calc')
+var remove= document.getElementById('remove')
+var tableEl = document.querySelector('table')
+
+//Variabe for the numbering
+var x= 2
 
 
-//Delete button
-var rem = document.getElementById('remove')
-var div = document.getElementById('example2')
-rem.addEventListener('click', function() {
-    //div.removeChild(div.lastChild)
-    div.parentNode.removeChild(div)
+//Function for the add button
+add.addEventListener('click', function() {
+    var new_row = tableEl.insertRow(tableEl.rows.length)
+
+    var cel1 = new_row.insertCell(0);
+    var cel2 = new_row.insertCell(1);
+    var cel3 = new_row.insertCell(2);
+    var cel4 = new_row.insertCell(3);
+
+
+    cel1.innerHTML = x++
+    cel2.innerHTML = '<input style="width: 150px; border: none;; background: transparent; text-align: center; font-size: 18px; border-bottom: 1px solid black">'
+    cel3.innerHTML = '<input style="width: 30px; font-size: 18px; text-align: center; background: transparent; border: none; border-bottom: 1px solid black">'
+    cel4.innerHTML = '<input style="width: 30px; font-size: 18px; text-align: center; background: transparent; border: none; border-bottom: 1px solid black">'
 })
+
+
+//Function for the delete button
+
+
+
+
+//rem.addEventListener('click', function() {
+    //div.removeChild(div.lastChild)
+    //div.parentNode.removeChild(div)
 
